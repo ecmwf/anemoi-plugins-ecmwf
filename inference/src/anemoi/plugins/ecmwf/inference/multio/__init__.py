4fca1f13209d112494032c6ff0e5e839e1e6309a
Harrison Cook <harrison.cook@ecmwf.int>
2026-09-14 16:01:27 +0000

    feat: enable setup of gribjump through FDB_ENABLE_GRIBJUMP

--- inference/src/anemoi/plugins/ecmwf/inference/multio/__init__.py
+++ inference/src/anemoi/plugins/ecmwf/inference/multio/__init__.py
@@ -10,4 +10,19 @@
 """Anemoi Inference Output Multio Plugin"""
 
 
+import logging
+import os
+
 from .multio_output import MultioOutputPlugin as MultioOutputPlugin
+
+LOG = logging.getLogger(__name__)
+
+
+if os.getenv("FDB_ENABLE_GRIBJUMP", "0") == "1":
+    # As gribjump through a wheel install cannot be auto discovered by fdb, it must be imported here
+    try:
+        import pygribjump  # type: ignore
+
+        LOG.info("gribjump version: %s", pygribjump.__version__)
+    except ImportError:
+        LOG.error("gribjump is not installed. FDB writes will not be indexed.")
